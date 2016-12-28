369917284cde820597a5431f5881654fa7db9a11
TomMGrello <tommgrello@gmail.com>
2016-12-27 21:04:57 -0500

    tried to do cannon stuff

---
+++ Assets/CannonAim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonAim : MonoBehaviour {
+	private bool rotating = false;
+	private Vector3 rotatePos;
+	public float rotatingSpeed = 0.5f;
+	// Update is called once per frame
+	void Update () {
+		if (GameObjectManager.buildModeScript.enabled == false) {
+			LookToClick ();
+		}
+	}
+
+	void LookToClick() {
+		if (Input.GetMouseButton (1)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				rotatePos = hit.point;
+				rotating = true;
+			}
+		}
+		if (rotating) {
+			Quaternion rotationNeeded = Quaternion.FromToRotation (transform.position, rotatePos);
+			Quaternion.RotateTowards (transform.rotation, rotationNeeded, rotatingSpeed);
+			if (transform.rotation.Equals(rotationNeeded))
+				rotating = false;
+		}
+	}
+}
